13703167833891299d2fb77eefd7ed97e95c3583
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-01-05 17:58:42 -0500

    add lightbnb_webapp folder

--- 1_queries/5_all_my_reservations.sql
+++ 1_queries/5_all_my_reservations.sql
@@ -1,4 +1,4 @@
-SELECT properties.*, reservations.*, avg(rating) as average_rating
+SELECT properties.title,reservations.start_date, properties.cost_per_night, avg(rating) as average_rating
 FROM reservations
 JOIN properties ON reservations.property_id = properties.id
 JOIN property_reviews ON properties.id = property_reviews.property_id
