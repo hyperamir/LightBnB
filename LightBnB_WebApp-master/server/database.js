00303e9305804a493208c445f5113566578d02e9
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-01-06 15:45:24 -0500

    addProperty function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -110,7 +110,6 @@ exports.getAllReservations = getAllReservations;
 const getAllProperties = function (options, limit = 10) {
 
   const queryParams = [];
-  console.log('option: ', options)
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
@@ -157,8 +156,6 @@ const getAllProperties = function (options, limit = 10) {
   LIMIT $${queryParams.length};
   `;
 
-  console.log('this is querry:', queryString);
-  console.log('this is parameters:', queryParams)
   return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 exports.getAllProperties = getAllProperties;
@@ -170,9 +167,21 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryString = `
+    INSERT INTO properties
+    (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+    VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14, true) RETURNING *;
+  `;
+
+  const queryParams = [property.owner_id, property.title, property.description, property.thumbnail_photo_url,
+  property.cover_photo_url, property.cost_per_night * 100, property.parking_spaces, property.number_of_bathrooms, 
+  property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code];
+
+  return pool.query(queryString, queryParams)
+  .then((result) => result.rows[0])
+    .catch((err) => {
+      console.log(err.message);
+    });
+
 }
 exports.addProperty = addProperty;
